c7d5b0d178e572312b571d8927ecb93ec91486b8
Tomasz Weglinski <tweglins@akamai.com>
2018-11-27 16:45:33 +0100

    Generate JWT token with hardcoded data

--- jwt/command-generate-token.go
+++ jwt/command-generate-token.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/urfave/cli"
 )
 
@@ -13,6 +16,54 @@ var commandGenerateToken = cli.Command{
 	Flags:       []cli.Flag{},
 }
 
+type IEClaims struct {
+	customClaims map[string]string
+	jwt.StandardClaims
+}
+
+type Token struct {
+	Raw       string                 // The raw token.  Populated when you Parse a token
+	Method    jwt.SigningMethod          // The signing method used or to be used
+	Header    map[string]interface{} // The first segment of the token
+	Claims    jwt.Claims                 // The second segment of the token
+	Signature string                 // The third segment of the token.  Populated when you Parse a token
+	Valid     bool                   // Is the token valid?  Populated when you Parse/Verify a token
+}
+
 func callGenerateToken(c *cli.Context) error {
+	var signingKey = []byte("AllYourBase") // TODO get from file? stdout?
+
+	var customClaims = map[string]string{ // TODO get from context
+		"clientId": "username",
+		"authGroups": "testGroup",
+	}
+	claims := constructClaims(customClaims)
+	token, err := createToken(claims, signingKey)
+
+	if err != nil {
+		fmt.Errorf("Error generating token %v", err)
+	} else {
+		fmt.Println("JWT Token:", token)
+	}
+
 	return nil
+}
+
+func constructClaims(customClaims map[string]string) IEClaims {
+	return IEClaims{
+		customClaims,
+		jwt.StandardClaims{
+			ExpiresAt: 15000,
+		},
+	}
+}
+
+func createToken(claims IEClaims, signingKey []byte) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(signingKey)
+
+	if err != nil {
+		return "", errors.New("Error generating token!")
+	}
+	return ss, nil
 }
---
+++ jwt/command-generate-token_test.go
@@ -0,0 +1,9 @@
+package main
+
+import "testing"
+
+func callGenerateTokenTest(t *testing.T) {
+
+
+
+}
