51bdce55c0ec77db80d4354ea23264bbcf4a0a30
Blazej Bucko <blazej.bucko@gmail.com>
2018-11-28 17:56:43 +0100

    Added dummy operation for IEC Configure

--- iec/command-configure.go
+++ iec/command-configure.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/akamai/cli-common-golang"
 	"github.com/bbucko/cli-iec/common"
 	"github.com/urfave/cli"
 	"log"
+	"time"
 )
 
 var commandConfigure = cli.Command{
@@ -12,7 +14,41 @@ var commandConfigure = cli.Command{
 	Description: "",
 	HideHelp:    true,
 	Action:      callConfigure,
-	Flags:       []cli.Flag{},
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "namespace",
+			Usage: "Namespace",
+			Value: "ns",
+		}, cli.StringFlag{
+			Name:  "jurisdiction",
+			Usage: "Jurisdiction (EU, NA, JP)",
+			Value: "EU",
+		}, cli.StringFlag{
+			Name:  "auth",
+			Usage: "Authentication method",
+			Value: "jwt",
+		}, cli.StringFlag{
+			Name:  "hostname",
+			Usage: "Hostname that clients will connect to",
+			Value: "iec.kwapiszewski.com",
+		}, cli.BoolFlag{
+			Name:  "mqtt",
+			Usage: "Enable MQTT over TLS",
+		}, cli.BoolFlag{
+			Name:  "ws",
+			Usage: "Enable MQTT over WebSockets",
+		}, cli.BoolFlag{
+			Name:  "https",
+			Usage: "Enable Gateway",
+		}, cli.StringFlag{
+			Name:  "jwtKey",
+			Usage: "Name of the JWT Key that will be used for generating/validating authentication tokens",
+			Value: "default",
+		}, cli.BoolFlag{
+			Name:  "activate",
+			Usage: "If present this configuration will be activated",
+		},
+	},
 }
 
 func callConfigure(c *cli.Context) error {
@@ -23,7 +59,14 @@ func callConfigure(c *cli.Context) error {
 		return err
 	}
 
-	log.Print("Config: ", config)
+	log.Print("Current configuration: ", config)
+
+	dummyOperation("Uploading keys to JWT")
+	dummyOperation("Configuring new Property")
+	dummyOperation("Activating Property")
+	dummyOperation("Creating new namespace in IEC")
+	dummyOperation("Updating topics")
+	dummyOperation("Activating namespace")
 
 	err = common.SaveConfig(c, config)
 	if err != nil {
@@ -32,4 +75,10 @@ func callConfigure(c *cli.Context) error {
 	}
 
 	return nil
+}
+
+func dummyOperation(operation string) {
+	akamai_cli.StartSpinner(operation, "Done")
+	time.Sleep(1 * time.Second)
+	akamai_cli.StopSpinnerOk()
 }
